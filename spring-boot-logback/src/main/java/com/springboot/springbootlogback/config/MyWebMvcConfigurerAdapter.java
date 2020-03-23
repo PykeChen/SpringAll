161e922ac60383c0bba7a6e46ef74b18d03866a5
cpy <cpy@meitu.com>
2020-03-23 14:42:40 +0800

    【优化】修复resource找不到的问题resource not find. 新版springboot实现implements WebMvcConfigurer接口

--- spring-boot-logback/src/main/java/com/springboot/springbootlogback/config/MyWebMvcConfigurerAdapter.java
+++ spring-boot-logback/src/main/java/com/springboot/springbootlogback/config/MyWebMvcConfigurerAdapter.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
+/**                        sss
  * Created by tengj on 2017/3/13.
  */
 @Configuration
-public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
+public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
 
 
     /**
@@ -22,11 +22,11 @@ public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //自定义项目内目录
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/my/");
+        registry.addResourceHandler("/my/**").addResourceLocations("classpath:/my/");
         //指向外部目录
 //        registry.addResourceHandler("/my/**").addResourceLocations("file:/Users/cpy/Downloads/Spring-project/SpringAll/spring-boot-logback/src/main/resources/my");
-        super.addResourceHandlers(registry);
     }
+
 
     /**
      * 以前要访问一个页面需要先创建个Controller控制类，在写方法跳转到页面
@@ -37,7 +37,6 @@ public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/toLogin").setViewName("login");
-        super.addViewControllers(registry);
     }
 
     /**
@@ -49,6 +48,5 @@ public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
         // addPathPatterns 用于添加拦截规则
         // excludePathPatterns 用户排除拦截
         registry.addInterceptor(new MyInterceptor()).addPathPatterns("/**").excludePathPatterns("/toLogin","/login");
-        super.addInterceptors(registry);
     }
 }
