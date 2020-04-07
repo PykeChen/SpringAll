174eaac08e8b1b14aa55b46b97f6470d4f49c3ce
cpy <cpy@meitu.com>
2020-04-07 11:15:44 +0800

    【feature】升级版本、更新配置

--- 21.Spring-Boot-Actuator/src/main/java/com/springboot/demo/DemoApplication.java
+++ 21.Spring-Boot-Actuator/src/main/java/com/springboot/demo/DemoApplication.java
@@ -1,5 +1,10 @@
 package com.springboot.demo;
 
+import com.springboot.demo.beans.DemoBeansTest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class DemoApplication {
+	public static Logger mLogger = LoggerFactory.getLogger("ActuatorLogger");
+
+	@Autowired
+	private DemoBeansTest mBeansTest;
+
 
 	@RequestMapping("/")
 	String index() {
@@ -16,5 +26,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		mLogger.error("Actuator demo application log tag = %s");
 	}
 }
